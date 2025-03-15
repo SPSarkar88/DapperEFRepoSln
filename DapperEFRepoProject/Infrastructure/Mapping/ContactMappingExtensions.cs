eae91f79712ff89e924f2dd5ef2e4108fa9e2346
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2025-03-15 10:33:50 +0530

    Add ContactMappingExtensions for contact object mappings
    Introduce ContactMappingExtensions.cs with methods to map
    CreateContactRequest, UpdateContactRequest, and Contact objects
    to Contact and ContactResponse types. Add necessary namespaces
    and set timestamps during conversions.

---
+++ DapperEFRepoProject/Infrastructure/Mapping/ContactMappingExtensions.cs
@@ -0,0 +1,78 @@
+﻿using DapperEFRepoProject.Models;
+using DapperEFRepoProject.Modules.Contacts.Request;
+using DapperEFRepoProject.Modules.Contacts.Response;
+
+namespace DapperEFRepoProject.Infrastructure.Mapping
+{
+    /// <summary>
+    /// Represents the extension methods for mapping contacts.
+    /// </summary>
+    public static class ContactMappingExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="CreateContactRequest"/> to a <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static Contact ToContact(this CreateContactRequest request)
+        {
+            return new Contact
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                Address = request.Address,
+                CreatedAt = DateTime.UtcNow // Set to current UTC time: 2025-03-14 16:31:45
+            };
+        }
+        /// <summary>
+        /// Updates a <see cref="Contact"/> from a <see cref="UpdateContactRequest"/>.
+        /// </summary>
+        /// <param name="contact"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static Contact UpdateFrom(this Contact contact, UpdateContactRequest request)
+        {
+            contact.FirstName = request.FirstName;
+            contact.LastName = request.LastName;
+            contact.Email = request.Email;
+            contact.PhoneNumber = request.PhoneNumber;
+            contact.Address = request.Address;
+            contact.UpdatedAt = DateTime.UtcNow; // Set to current UTC time: 2025-03-14 16:31:45
+
+            return contact;
+        }
+        /// <summary>
+        /// Converts a <see cref="Contact"/> to a <see cref="ContactResponse"/>.
+        /// </summary>
+        /// <param name="contact"></param>
+        /// <returns></returns>
+        public static ContactResponse ToContactResponse(this Contact contact)
+        {
+            return new ContactResponse
+            {
+                Id = contact.Id,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+                Email = contact.Email,
+                PhoneNumber = contact.PhoneNumber,
+                Address = contact.Address,
+                CreatedAt = contact.CreatedAt,
+                UpdatedAt = contact.UpdatedAt
+            };
+        }
+        /// <summary>
+        /// Converts a collection of <see cref="Contact"/> to a <see cref="ContactListResponse"/>.
+        /// </summary>
+        /// <param name="contacts"></param>
+        /// <returns></returns>
+        public static ContactListResponse ToContactListResponse(this IEnumerable<Contact> contacts)
+        {
+            return new ContactListResponse
+            {
+                Contacts = contacts.Select(c => c.ToContactResponse()).ToList()
+            };
+        }
+    }
+}
