1a4aa8bf9abed6f5cc74c584ee341339df020378
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2025-03-15 22:44:58 +0530

    Add DeleteContactRequest struct in new namespace
    A new namespace `DapperEFRepoProject.Modules.Contacts.Request` has been added.
    Within this namespace, a new struct `DeleteContactRequest` has been introduced.
    The `DeleteContactRequest` struct includes:
    - A parameterless constructor.
    - A property `Id` of type `int` with both getter and setter methods.
    The struct is documented with XML comments, including a summary for the struct itself and its constructor, as well as for the `Id` property.

---
+++ DapperEFRepoProject/Modules/Contacts/Request/DeleteContactRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace DapperEFRepoProject.Modules.Contacts.Request
+{
+    /// <summary>
+    /// Represents the request to update a contact.
+    /// </summary>
+    public struct DeleteContactRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteContactRequest"/> struct.
+        /// </summary>
+        public DeleteContactRequest()
+        {
+        }
+        /// <summary>
+        /// Gets or sets the id of the contact.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
