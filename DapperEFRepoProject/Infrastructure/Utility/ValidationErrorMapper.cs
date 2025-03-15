2ef58a7c69776897eb6a6e58affab2ddce54e249
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2025-03-15 20:18:15 +0530

    Add ValidationErrorMapper to map validation errors
    Introduce ValidationErrorMapper class in DapperEFRepoProject.Infrastructure.Utility namespace.
    Add MapValidationError method to convert ValidationFailure to ValidationError.
    Include necessary using directives for DapperEFRepoProject.Modules.Common.Response and FluentValidation.Results.

---
+++ DapperEFRepoProject/Infrastructure/Utility/ValidationErrorMapper.cs
@@ -0,0 +1,26 @@
+﻿using DapperEFRepoProject.Modules.Common.Response;
+using FluentValidation.Results;
+using static DapperEFRepoProject.Modules.Common.Response.ErrorResponse;
+
+namespace DapperEFRepoProject.Infrastructure.Utility
+{
+    /// <summary>
+    /// Represents a validation error mapper.
+    /// </summary>
+    public static class ValidationErrorMapper
+    {
+        /// <summary>
+        /// Maps a validation error to a response error.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static ValidationError MapValidationError(ValidationFailure e)
+        {
+            return new ValidationError
+            {
+                PropertyName = e.PropertyName,
+                ErrorMessage = e.ErrorMessage
+            };
+        }
+    }
+}
