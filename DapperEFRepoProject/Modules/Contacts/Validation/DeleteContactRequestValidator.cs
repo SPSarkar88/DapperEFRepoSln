623d014478b3a474e3e9e6f7af050f75fa6339d6
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2025-03-15 22:44:27 +0530

    Add request validators for contact operations
    Introduce new validation classes for handling requests in the DapperEFRepoProject module:
    - `CreateContactRequestValidator`: Validates `FirstName`, `LastName`, `Email`, and `PhoneNumber` fields.
    - `DeleteContactRequestValidator`: Ensures `Id` is not empty, not null, and greater than 0.
    - `UpdateContactRequestValidator`: Applies similar rules as `CreateContactRequestValidator`.

---
+++ DapperEFRepoProject/Modules/Contacts/Validation/DeleteContactRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using DapperEFRepoProject.Modules.Contacts.Request;
+
+namespace DapperEFRepoProject.Modules.Contacts.Validation
+{
+    public class DeleteContactRequestValidator : AbstractValidator<DeleteContactRequest>
+    {
+        public DeleteContactRequestValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
