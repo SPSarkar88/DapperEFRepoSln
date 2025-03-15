4788708050df85655d289347eeea11fccd4e060d
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2025-03-15 22:46:03 +0530

    Add XML documentation to DeleteContactRequestValidator
    Added XML documentation comments to the DeleteContactRequestValidator class and its constructor. These comments improve readability and maintainability by explaining the purpose and functionality of the class and its members.

--- DapperEFRepoProject/Modules/Contacts/Validation/DeleteContactRequestValidator.cs
+++ DapperEFRepoProject/Modules/Contacts/Validation/DeleteContactRequestValidator.cs
@@ -3,8 +3,14 @@ using DapperEFRepoProject.Modules.Contacts.Request;
 
 namespace DapperEFRepoProject.Modules.Contacts.Validation
 {
+    /// <summary>
+    /// Represents the validator for the <see cref="DeleteContactRequest"/> struct.
+    /// </summary>
     public class DeleteContactRequestValidator : AbstractValidator<DeleteContactRequest>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteContactRequestValidator"/> class.
+        /// </summary>
         public DeleteContactRequestValidator()
         {
             RuleFor(x => x.Id).NotEmpty().NotNull().GreaterThan(0);
