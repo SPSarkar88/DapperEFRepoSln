f484ce1a3297a9bbc234fbef12528857c44093ed
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2025-03-16 10:27:15 +0530

    Add GetContactById module and update project file
    Modified `DapperEFRepoProject.csproj` to remove the `Modules\Contacts\Query` folder.
    Added `GetContactById` class in `GetContactById.cs` under `DapperEFRepoProject.Modules.Contacts.Query` namespace.
    The class uses the `Carter` library to define a module, includes dependencies on `ILogger<GetContactById>`, `IContactService`, and `IValidator<DeleteContactRequest>`, and maps a GET endpoint `/api/contacts/{id:int}` to the `GetContactByIdAsync` method.

---
+++ DapperEFRepoProject/Modules/Contacts/Query/GetContactById.cs
@@ -0,0 +1,60 @@
+﻿using Carter;
+using DapperEFRepoProject.Modules.Contacts.Request;
+using DapperEFRepoProject.Modules.Contacts.Service;
+using FluentValidation;
+
+namespace DapperEFRepoProject.Modules.Contacts.Query
+{
+    /// <summary>
+    /// Represents the get contact by id module.
+    /// </summary>
+    public class GetContactById : CarterModule
+    {
+        /// <summary>
+        /// The logger used to log information to the console.
+        /// </summary>
+        private readonly ILogger<GetContactById> _logger;
+        /// <summary>
+        /// The contact service used to interact with the database.
+        /// </summary>
+        private readonly IContactService _contactService;
+        /// <summary>
+        /// The validator used to validate the request.
+        /// </summary>
+        private readonly IValidator<DeleteContactRequest> _validator;
+        public GetContactById(
+            ILogger<GetContactById> logger, 
+            IContactService contactService, 
+            IValidator<DeleteContactRequest> validator)
+        {
+            _logger = logger;
+            _contactService = contactService;
+            _validator = validator;
+        }
+        /// <summary>
+        /// Adds the routes to the module.
+        /// </summary>
+        /// <param name="app"></param>
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/contacts/{id:int}", GetContactByIdAsync);
+        }
+        /// <summary>
+        /// Gets a contact by its unique identifier.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async ValueTask<IResult> GetContactByIdAsync(HttpContext context, int id)
+        {
+            _logger.LogInformation("Getting contact with id {Id}", id);
+            var contact = await _contactService.GetContactAsync(id);
+            if (contact is null)
+            {
+                return Results.NotFound();
+            }
+            _logger.LogInformation("Getting contact with id {Id} successful", id);
+            return Results.Ok(contact);
+        }
+    }
+}
